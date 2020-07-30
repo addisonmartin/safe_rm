1fa37f61b6ae949909169bd8e032d6322f024116
Addison Martin <addisonejmartin@gmail.com>
2020-07-30 18:18:04 -0400

    Working on implementing the test scripts functionality, and some simple test cases.

--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
 
+test_inputs_directory="$PWD/tests/inputs"
+safe_rm_directory="$PWD/safe_rm"
+
+for test_input_file in "$test_inputs_directory"/*.input; do
+
+  test_input=$(cat "$test_input_file")
+
+  actual_test_output_file=${test_input_file/inputs/actual_outputs}
+  actual_test_output_file=${$actual_test_output_file/input/output}
+
+  "$safe_rm_directory" "$test_input" >> "$actual_test_output_file"
+
+  # expected_test_output_file=${test_input_file/inputs/expected_outputs}
+  # expected_test_output_file=${expected_test_output_file/input/output}
+
+  # diff --minimal "$actual_test_output_file" "$expected_test_output_file"
+done
